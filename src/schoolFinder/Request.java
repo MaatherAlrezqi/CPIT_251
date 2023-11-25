21648a4cbfb0e4186ac805b69c36e481e75c4cce
sara_alharbi <sususu965@gmail.com>
2023-11-25 15:55:59 +0300

    request class

--- src/schoolFinder/Request.java
+++ src/schoolFinder/Request.java
@@ -2,16 +2,32 @@
 package schoolFinder;
 
 class Request {
+    private static int nextRequestId = 1;
+    private int requestId;
     private Child child;
     private School school;
     private String status;
 
-    public Request(Child child, School school, String status) {
-        this.child = child;
-        this.school = school;
-        this.status = status;
+public Request(int requestId ,Child child, School school, String status) {
+    this.requestId = requestId;
+    this.child = child;
+    this.school = school;
+    this.status = status;
+}
+     //  method to generate the next request ID
+    public static int generateRequestId() {
+        return nextRequestId++;
+    }
+        public String toFormattedString() {
+        return String.format("Request ID: %d Child ID: %d School: %s Status: %s", requestId, child.getChildId(), school.getName(), status);
     }
 
+    public String toFormattedStringForFile() {
+        return String.format("%d,%d,%s,%s", requestId, child.getChildId(), school.getName(), status);
+    }
+      public int getRequestId() {
+        return requestId;
+    }
     public Child getChild() {
         return child;
     }
@@ -35,4 +51,4 @@ class Request {
     public void setStatus(String status) {
         this.status = status;
     }
-}
+    }
